08b6eecdefa9803bd0c08f4c0498d98bb18d1ee0
NWChen <neilwchen@gmail.com>
2014-07-22 21:06:47 -0400

    Adding to Midterm materials

--- Midterm/problem1.cpp
+++ Midterm/problem1.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 void f(int i){
-    if(i == 4||i == 5) return; //if assignment, e.g. (i=4||i=5), then no output is produced; those statements would always resolve to true
+    if(i == 4||i == 5) return; //if assignment, e.g. (i=4||i=5), then no output is produced; those statements would always resolve to truey"
     cout << "hello world\n" ;
 }
 
---
+++ Midterm/problem3.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cstdlib>
+#include <assert.h>
+using namespace std;
+
+bool ok(int q[], int col){
+    for(int i=0; i<col; i++)
+        if(q[col]==q[i] || (col-i)==abs(q[col]-q[i])) return false;
+    return true;
+};
+
+int main(){
+    int a[8]={1, 2, 3, 4, 5, 6, 7, 0};
+    int b[8]={2, 2, 1, 7, 8, 5, 3, 4};
+    int c[8]={7, 3, 0, 2, 5, 1, 6, 4};
+    assert(!(ok(a,3)));
+    assert(!(ok(b,7)));
+    assert(ok(c,7));
+    return 0;
+}

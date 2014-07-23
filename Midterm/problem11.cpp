ecea667c9c6041e1d1b74f755c1975c043b65033
Neil Chen <neilwchen@gmail.com>
2014-07-23 17:42:47 -0400

    added problem11

---
+++ Midterm/problem11.cpp
@@ -0,0 +1,29 @@
+/*
+Write a function
+int number_of_primes(int from, int to)
+that returns the number of prime numbers between from and to.
+*/
+
+#include <iostream>
+using namespace std;
+
+bool isPrime(int n){
+    if(n<=1) return false;
+    for(int i=2; i<n; i++){
+        if(n%i==0) return false;
+    }
+    return true;
+};
+
+int number_of_primes(int from, int to){
+    int counter=0;
+    for(int i=from; i<to; i++){
+        if(isPrime(i)) counter++;
+    }
+    return counter;
+};
+
+int main(){
+    cout << number_of_primes(1,1000); //168
+    return 0;
+};
